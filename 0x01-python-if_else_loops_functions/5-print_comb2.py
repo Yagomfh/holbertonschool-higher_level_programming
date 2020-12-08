61ea6efc250eb29bec2225b97733da94f7d97a12
yagomfh <yagomfh@gmail.com>
2020-12-08 13:08:44 +0000

    fix(2-prin..) : print str format only

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print(0, end='')
+        print("{}".format(0), end='')
     if i < 99:
         print("{}".format(i), end=", ")
     else:
-        print(i)
+        print("{}".format(i))
