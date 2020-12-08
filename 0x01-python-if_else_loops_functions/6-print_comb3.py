cd0f77495bae998a6cf529127fa4b989249a6961
yagomfh <yagomfh@gmail.com>
2020-12-08 13:12:53 +0000

    fix(2-prin..) : print str format only

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,7 +5,7 @@ while i < 10:
     j = 1 + a
     while j < 10:
         if i == 8 and j == 9:
-            print(89)
+            print("{}".format(89))
         else:
             print("{}{}, ".format(i, j), end='')
         j += 1
