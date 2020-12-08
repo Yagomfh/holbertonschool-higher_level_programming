665ad18ba09e7acf72368ee41f88b74c065c9adb
yagomfh <yagomfh@gmail.com>
2020-12-08 10:59:56 +0000

    feat(8_upp..) : 1st commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) > 96:
+            c = chr(ord(str[i]) - 32)
+        else:
+            c = str[i]
+        print(c, end='' if i + 1 < len(str) else "\n")
