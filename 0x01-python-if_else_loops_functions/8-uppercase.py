c0d571f9410df7acc09f4c3371ce8dcbf2ec1f67
yagomfh <yagomfh@gmail.com>
2020-12-08 13:27:39 +0000

    fix() : new commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def uppercase(str):
     tmp = ""
     for c in str:
-        if ord(c) > 96:
+        if ord(c) >= 97 and ord(c) <= 122:
             tmp = tmp + chr(ord(c) - 32)
         else:
             tmp = tmp + c
