d4ac53ede29f6853a2b83b1a1eb76fb68050b09d
yagomfh <yagomfh@gmail.com>
2020-12-08 10:09:25 +0000

    feat(6-pri..) : 1st commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+i = 0
+a = 0
+while i < 10:
+    j = 1 + a
+    while j < 10:
+        if i == 8 and j == 9:
+            print(89)
+        else:
+            print("{}{}, ".format(i, j), end='')
+        j += 1
+    i += 1
+    a += 1
