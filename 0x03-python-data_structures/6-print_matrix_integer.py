65f536ccbe33f585a676c77e1177ec0ebd769a5b
yagomfh <yagomfh@gmail.com>
2020-12-15 13:32:54 +0000

    fix: requirements

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     col = len(matrix[0])
     if row == 1 and col == 0:
         print("")
-    for i in range(0, row):
-        for j in range(0, col):
-            print("{:d}".format(matrix[i][j]), end=" " if j != 2 else "\n")
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end=" " if i != 2 else "\n")
