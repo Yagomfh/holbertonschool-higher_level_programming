b07d15c2555aecb3c50e29a47c829dec867ed9ab
yagomfh <yagomfh@gmail.com>
2020-12-15 13:23:46 +0000

    fix: not using replace

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    c = my_string.count('c')
-    while c > 0:
-        my_string = my_string.replace('c', '')
-        c -= 1
-    C = my_string.count('C')
-    while C > 0:
-        my_string = my_string.replace('C', '')
-        C -= 1
-    return my_string
+    res = ""
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        res += i
+    return res
