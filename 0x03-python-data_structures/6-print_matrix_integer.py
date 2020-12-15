ff45e345b8623b2e8f87e6021ac73e387a77f90a
yagomfh <yagomfh@gmail.com>
2020-12-15 13:34:53 +0000

    fix: adding first line

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     row = len(matrix)
     col = len(matrix[0])
     if row == 1 and col == 0:
         print("")
-    for row in matrix:
-        for i in range(len(row)):
-            print("{:d}".format(row[i]), end=" " if i != 2 else "\n")
+    for i in range(0, row):
+        for j in range(0, col):
+            print("{:d}".format(matrix[i][j]), end=" " if j != 2 else "\n")
