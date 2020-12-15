ee50f6b82b4480df7e1f83eb8bd05f4cba47e5c5
yagomfh <yagomfh@gmail.com>
2020-12-15 13:25:45 +0000

    fix: not casting

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
         print("")
     for i in range(0, row):
         for j in range(0, col):
-            print("{}".format(matrix[i][j]), end=" " if j != 2 else "\n")
+            print("{:d}".format(matrix[i][j]), end=" " if j != 2 else "\n")
