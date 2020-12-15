3705254785b571ca0d56f00f6eead35176d7f709
yagomfh <yagomfh@gmail.com>
2020-12-15 14:11:59 +0000

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
         print("")
     for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[0])):
+        for j in range(0, len(matrix[i])):
             if j == len(matrix) - 1:
                 print("{:d}".format(matrix[i][j]))
             else:
