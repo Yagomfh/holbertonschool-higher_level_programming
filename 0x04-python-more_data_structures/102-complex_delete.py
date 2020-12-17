dd3603394de07a2cb8cc81342ca7d6e18d3c4f02
yagomfh <yagomfh@gmail.com>
2020-12-17 16:06:28 +0000

    fix: pep8 complaint

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     todel = []
-    for k,v in a_dictionary.items():
+    for k, v in a_dictionary.items():
         if v == value:
-            todel.append(k);
+            todel.append(k)
     if todel:
         for key in todel:
             del a_dictionary[key]
