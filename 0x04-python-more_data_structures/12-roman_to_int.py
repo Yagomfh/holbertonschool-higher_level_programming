0f6db23ca1149b1772e94762cef61a26bc17c05a
yagomfh <yagomfh@gmail.com>
2020-12-17 14:43:36 +0000

    fix: return 0 instead of None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,19 +7,18 @@ def find_bigger_number(tocheck, nb, idx):
 
 
 def roman_to_int(roman_string):
+    res = 0
     if roman_string and type(roman_string) is str:
         rlist = list(roman_string)
         rdi = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        alist = []
         res = 0
-        for nb in rlist:
-            for lt in rdi:
-                if lt == nb:
-                    alist.append(rdi[lt])
-        for nb in range(0, len(alist)):
-            if find_bigger_number(alist, alist[nb], nb) is True:
-                res -= alist[nb]
+        for i in range(len(roman_string) - 1):
+            c = roman_string[i]
+            c_next = roman_string[i + 1]
+            if rdi[c] > rdi[c_next]:
+                res -= rdi[c]
             else:
-                res += alist[nb]
+                res += rdi[c]
+        res += rdi[roman_string[len(roman_string) - 1]]
         return res
-    return None
+    return res
