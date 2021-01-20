eb974cec2793021202cf4f8728983ce90d0afa7e
yagomfh <yagomfh@gmail.com>
2021-01-20 15:51:44 +0000

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,3 @@ def read_file(filename=""):
     """Function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as File:
         print(File.read())
-        File.close()
