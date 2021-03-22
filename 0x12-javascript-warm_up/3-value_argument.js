53b614f97b7532b69dbdd50d110115dc4d130a57
yagomfh <yagomfh@gmail.com>
2021-03-22 11:14:07 +0000

    fix: using undefined

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
 // prints the first argument passed to it
 
-const process = require('process');
-
 const args = process.argv;
-let count = 0;
 
-while (args[count] !== undefined) {
-  count++;
-}
-
-if (count === 3) {
+if (args[2] !== undefined) {
   console.log(args[2]);
-}
-if (count < 3) {
+} else {
   console.log('No argument');
 }
