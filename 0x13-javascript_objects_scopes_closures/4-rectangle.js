2a768e73506a1f3b6eb4376b6173c0119ebcd828
yagomfh <yagomfh@gmail.com>
2021-03-23 13:17:02 +0000

    fix: without import

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,15 +1,31 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-Rectangle.prototype.rotate = function () {
-  const tmp = this.width;
-  this.width = this.height;
-  this.height = tmp;
+  print () {
+    let row = 'X';
+    let i;
+    for (i = this.width; i > 1; i--) {
+      row += 'X';
+    }
+    for (i = this.height; i > 0; i--) {
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
-
-Rectangle.prototype.double = function () {
-  this.width *= 2;
-  this.height *= 2;
-};
-
-module.exports = Rectangle;
