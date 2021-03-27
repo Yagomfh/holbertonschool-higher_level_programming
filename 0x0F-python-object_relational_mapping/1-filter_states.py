05df77d8f28553e66a172ff68ee5de85d1f2919d
yagomfh <yagomfh@gmail.com>
2021-03-27 09:11:09 +0000

    fix: all code in name == main

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,18 +4,12 @@ import MySQLdb
 import sys
 
 
-db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-
-
-def sql_query():
-    """SQL query"""
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
     states = cur.execute("SELECT * FROM states ORDER BY id")
     result = cur.fetchall()
     for row in result:
         if row[1][0] == 'N':
             print(row)
-
-if __name__ == '__main__':
-    sql_query()
