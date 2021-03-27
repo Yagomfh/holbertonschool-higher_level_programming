71c9c98d9de7d31bb3e2eb571bbddca2dc9fed21
yagomfh <yagomfh@gmail.com>
2021-03-27 09:30:29 +0000

    fix: indentation error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,9 +4,7 @@ import MySQLdb
 import sys
 
 
-def sql_query():
-    """Sql query"""
-    if __name__ == '__main__':
+if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
