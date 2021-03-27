4d59cad78c5f57ca93946e60a3ea4c96016207b7
yagomfh <yagomfh@gmail.com>
2021-03-27 09:40:10 +0000

    fix: all code in name == main

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,14 +7,14 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 from sqlalchemy.engine.url import URL
 
-db = {'drivername': 'mysql+mysqldb',
-      'host': 'localhost',
-      'port': '3306',
-      'username': sys.argv[1],
-      'password': sys.argv[2],
-      'database': sys.argv[3]}
 
 if __name__ == "__main__":
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': sys.argv[1],
+          'password': sys.argv[2],
+          'database': sys.argv[3]}
     url = URL(**db)
     engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
