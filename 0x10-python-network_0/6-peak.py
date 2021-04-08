6db660b21be7038dbf023f511cf61537171467bc
yagomfh <yagomfh@gmail.com>
2021-04-08 11:43:31 +0000

    feat: initial solution

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,25 +2,22 @@
 """Finds a peak"""
 
 
-def peak(arr, low, high, n):
-    """Recursive function to find peak"""
-    mid = low + (high - low) / 2
-    mid = int(mid)
-
-    if ((mid == 0 or arr[mid - 1] <= arr[mid]) and
-       (mid == n - 1 or arr[mid + 1] <= arr[mid])):
-        return mid
-
-    elif (mid > 0 and arr[mid - 1] > arr[mid]):
-        return peak(arr, low, (mid - 1), n)
-
-    else:
-        return peak(arr, (mid + 1), high, n)
-
-
 def find_peak(list_of_integers):
     """Wraper over recursive"""
     if list_of_integers == []:
         return None
-    n = len(list_of_integers) + 1
-    return peak(list_of_integers, 0, n - 1, n)
+    n = len(list_of_integers)
+
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return max(list_of_integers)
+
+    mid = int(n / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak > list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
