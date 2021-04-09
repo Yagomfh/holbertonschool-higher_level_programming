0df2869ec84456baef8da66ecd7eaf131a52147d
yagomfh <yagomfh@gmail.com>
2021-04-09 16:50:11 +0000

    fix: wrong url

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = 'http://577da9f9ae6a.0a98cdc3.hbtn-cod.io:5000/search_user'
+    url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) == 2:
         q = sys.argv[1]
     else:
