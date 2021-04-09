eba69d3385cad6f55372307732a1514200497655
yagomfh <yagomfh@gmail.com>
2021-04-09 12:22:34 +0000

    fix: error code added

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
-    if int(r.status_code) >= 400:
-        print(r.status_code)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
