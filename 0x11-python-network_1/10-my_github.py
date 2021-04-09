8756b4d2ad8845854557fb05a747d97c6ae65abf
yagomfh <yagomfh@gmail.com>
2021-04-09 16:42:26 +0000

    if __name__ == __main__: added

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,13 +4,13 @@ and uses the GitHub API to display your id"""
 import sys
 import requests
 
-
-username = sys.argv[1]
-password = sys.argv[2]
-url = "https://api.github.com/users/" + username
-data = {'Authorization': password}
-try:
-    r = requests.get(url, headers=data)
-    print(r.json()['id'])
-except:
-    print("None")
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/users/" + username
+    data = {'Authorization': password}
+    try:
+        r = requests.get(url, headers=data)
+        print(r.json()['id'])
+    except:
+        print("None")
