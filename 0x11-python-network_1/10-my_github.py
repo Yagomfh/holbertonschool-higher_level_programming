53f51efe748ce6e6b81ed45b560c293100763e92
yagomfh <yagomfh@gmail.com>
2021-04-09 16:46:57 +0000

    fix: new logic

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,13 +4,13 @@ and uses the GitHub API to display your id"""
 import sys
 import requests
 
+
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
-    url = "https://api.github.com/users/" + username
-    data = {'Authorization': password}
+    url = "https://api.github.com/user"
     try:
-        r = requests.get(url, headers=data)
+        r = requests.get(url, auth=(username, password))
         print(r.json()['id'])
     except:
         print("None")
